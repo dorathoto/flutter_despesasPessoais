1e564cd1b184e3dce7adea2a2e3a00a567237605
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2021-07-21 08:28:23 -0300

    inicio

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Despesas pessoais')),
+      body: Center(child: Text('Versão inicial')),
+    );
+  }
+}
