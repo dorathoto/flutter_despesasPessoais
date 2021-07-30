b7285fb9e41a71cdcc94fad771211bc009ad0217
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2021-07-30 08:34:23 -0300

    feat: themeData e Theme.of

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
     );
   }
 }
